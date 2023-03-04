0f56b196fe0ddb48b97f58a4d2d82d34b7eb7a0a
Abdullah Ahmed <abdullah.ahmed.a2000@gmail.com>
2023-03-04 04:35:55 +0200

    js hooks to ts

--- src/hooks/useOnClickOutside.ts
+++ src/hooks/useOnClickOutside.ts
@@ -1,8 +1,8 @@
 import { useEffect } from 'react'
 
-function useOnClickOutside (ref, handler) {
+function useOnClickOutside (ref: any, handler: any) {
   useEffect(() => {
-    const listener = event => {
+    const listener = (event: any) => {
       if (!ref.current || ref.current.contains(event.target)) {
         return
       }
