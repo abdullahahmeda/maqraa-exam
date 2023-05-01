c991beb630475ef8533e20033bc73b2cc67222d3
Abdullah Ahmed <abdullah.ahmed.a2000@gmail.com>
2023-05-01 14:54:23 +0300

    fix for chart

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -24,6 +24,7 @@ const DashboardPage = ({
   studentsCount,
   last30DaysExamsCounts
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  console.log(last30DaysExamsCounts)
   return (
     <>
       <Head>
@@ -32,7 +33,7 @@ const DashboardPage = ({
       <div>
         <ResponsiveContainer height={300}>
           <BarChart
-            data={last30DaysExamsCounts}
+            data={last30DaysExamsCounts as any[]}
             margin={{
               top: 0,
               right: 0,
@@ -57,13 +58,13 @@ const DashboardPage = ({
             <Legend />
             <Bar
               type='monotone'
-              dataKey='_count._all'
+              dataKey={obj => '' + Number(obj.count)}
               fill='#8884d8'
               name='الكل'
             />
             <Bar
               type='monotone'
-              dataKey='_count.grade'
+              dataKey={obj => '' + Number(obj.graded)}
               fill='#82ca9d'
               name='المصححة'
             />
@@ -84,16 +85,11 @@ export async function getServerSideProps (context: GetServerSidePropsContext) {
   })
   const examsCount = await prisma.exam.count()
   const ungradedExamsCount = await prisma.exam.count({ where: { grade: null } })
-  const last30DaysExamsCounts = await prisma.exam.groupBy({
-    by: ['createdAt'],
-    _count: {
-      _all: true,
-      grade: true
-    },
-    orderBy: {
-      createdAt: 'desc'
-    }
-  })
+  const last30DaysExamsCounts =
+    await prisma.$queryRaw`SELECT COUNT("public"."Exam"."grade") AS graded, COUNT(*), CAST("public"."Exam"."createdAt" AS DATE) FROM "public"."Exam" WHERE "public"."Exam"."createdAt" >= ${dayjs()
+      .subtract(30, 'day')
+      .startOf('day')
+      .toDate()} GROUP BY CAST("public"."Exam"."createdAt" AS DATE) ORDER BY CAST("public"."Exam"."createdAt" AS Date) ASC`
 
   return {
     props: {
