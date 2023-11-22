b2410019e10bf251dad17efd418993ea791919da
Abdullah <abdullah.ahmed.a2000@gmail.com>
2023-11-22 08:36:30 +0200

    a

--- src/validation/updateSettingsSchema.ts
+++ src/validation/updateSettingsSchema.ts
@@ -1,13 +1,15 @@
-import { SettingKey } from '../constants'
-import { z } from 'zod'
+export const UNUSED = true
 
-export const updateSettingsSchema = z.object({
-  [SettingKey.EASY_MCQ_QUESTIONS]: z.number().positive().int(),
-  [SettingKey.EASY_WRITTEN_QUESTIONS]: z.number().positive().int(),
-  [SettingKey.MEDIUM_MCQ_QUESTIONS]: z.number().positive().int(),
-  [SettingKey.MEDIUM_WRITTEN_QUESTIONS]: z.number().positive().int(),
-  [SettingKey.HARD_MCQ_QUESTIONS]: z.number().positive().int(),
-  [SettingKey.HARD_WRITTEN_QUESTIONS]: z.number().positive().int()
-})
+// import { SettingKey } from '../constants'
+// import { z } from 'zod'
 
-export type ValidSchema = z.infer<typeof updateSettingsSchema>
+// export const updateSettingsSchema = z.object({
+//   [SettingKey.EASY_MCQ_QUESTIONS]: z.number().positive().int(),
+//   [SettingKey.EASY_WRITTEN_QUESTIONS]: z.number().positive().int(),
+//   [SettingKey.MEDIUM_MCQ_QUESTIONS]: z.number().positive().int(),
+//   [SettingKey.MEDIUM_WRITTEN_QUESTIONS]: z.number().positive().int(),
+//   [SettingKey.HARD_MCQ_QUESTIONS]: z.number().positive().int(),
+//   [SettingKey.HARD_WRITTEN_QUESTIONS]: z.number().positive().int()
+// })
+
+// export type ValidSchema = z.infer<typeof updateSettingsSchema>
