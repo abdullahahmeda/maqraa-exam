c70bafd883eb1eac311310ecd27085b0f93e91fe
Abdullah <abdullah.ahmed.a2000@gmail.com>
2024-05-23 08:24:01 +0300

    freska

--- src/server/api/routers/model-question.ts
+++ src/server/api/routers/model-question.ts
@@ -4,7 +4,7 @@ import {
   protectedProcedure,
   publicProcedure,
 } from '~/server/api/trpc'
-import type { ExpressionBuilder, SelectQueryBuilder } from 'kysely'
+import type { ExpressionBuilder } from 'kysely'
 import type { DB } from '~/kysely/types'
 import { jsonObjectFrom } from 'kysely/helpers/postgres'
 
@@ -14,34 +14,32 @@ const includeSchema = z.object({
 
 type Include = z.infer<typeof includeSchema>
 
-function withQuestion(eb: ExpressionBuilder<DB, 'ModelQuestion'>) {
-  return jsonObjectFrom(
-    eb
-      .selectFrom('Question')
-      .selectAll('Question')
-      .whereRef('ModelQuestion.questionId', '=', 'Question.id'),
-  ).as('question')
-}
-
-function applyInclude<O>(
-  query: SelectQueryBuilder<DB, 'ModelQuestion', O>,
-  include: Include | undefined,
-) {
-  if (include === undefined) return query
-  return query.$if(!!include.question, (qb) => qb.select(withQuestion))
+export function applyInclude(include: Include | undefined) {
+  return (eb: ExpressionBuilder<DB, 'ModelQuestion'>) => {
+    return [
+      ...(include?.question
+        ? [
+            jsonObjectFrom(
+              eb
+                .selectFrom('Question')
+                .selectAll('Question')
+                .whereRef('ModelQuestion.questionId', '=', 'Question.id'),
+            ).as('question'),
+          ]
+        : []),
+    ]
+  }
 }
 
 export const modelQuestionRouter = createTRPCRouter({
   get: publicProcedure
     .input(z.object({ id: z.string(), include: includeSchema.optional() }))
     .query(async ({ input, ctx }) => {
-      const query = applyInclude(
-        ctx.db
-          .selectFrom('ModelQuestion')
-          .selectAll()
-          .where('id', '=', input.id),
-        input.include,
-      )
+      const query = ctx.db
+        .selectFrom('ModelQuestion')
+        .selectAll('ModelQuestion')
+        .select(applyInclude(input.include))
+        .where('id', '=', input.id)
 
       return query.executeTakeFirst()
     }),
