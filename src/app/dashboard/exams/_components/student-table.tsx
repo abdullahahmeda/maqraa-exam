b70df4e91f91c98fc5e722a43800d2a33b2849b2
Abdullah <abdullah.ahmed.a2000@gmail.com>
2024-05-29 21:23:13 +0300

    ad

--- src/app/dashboard/exams/_components/student-table.tsx
+++ src/app/dashboard/exams/_components/student-table.tsx
@@ -195,6 +195,7 @@ export const StudentQuizzesTable = ({
     <div>
       <DataTable
         data={quizzes.data}
+        // @ts-expect-error Can't type this
         columns={columns}
         columnFilters={{
           onColumnFiltersChange: setColumnFilters,
